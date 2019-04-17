f746e6b665a9dde26e1eb755fac53e19d8a4fdb3
phaybein <fabian.m.martinez@gmail.com>
2019-04-17 13:10:01 -0700

    completed styles for header RE #complete #header #styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import './Header.scss';
 import Navbar from '../Navbar/Navbar';
+import spiceImage from '../../assets/spice.png';
 
 export class Header extends Component {
   render() {
     return (
-      <header className='header bg-dark'>
+      <header className='header'>
         <Navbar />
+
+        <div className='container'>
+          <div className='header__welcome'>
+            <p>welcome</p>
+            <h2>prime steak restaurant</h2>
+          </div>
+
+          <div className='header__contact'>
+            <p className='contact__book'>Book a table directly</p>
+
+            <h3 className='contact__phone'>(714) 555-5555</h3>
+
+            <div className='contact__hours'>
+              <span className='hours__title'>Opening hours:</span>
+              <span className='hours__days'> Mon - Fri:</span>
+              <span className='hours__time'> 9:00 a.m. - 9:00 p.m. </span> /
+              <span className='hours__days'> Weekends:</span>
+              <span className='hours__time'> 9:00 a.m. - 10:00 p.m.</span>
+            </div>
+          </div>
+          <img src={spiceImage} alt='' className='header__spice' />
+        </div>
       </header>
     );
   }
